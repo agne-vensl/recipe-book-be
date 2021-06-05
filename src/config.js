d1bf6433405d70a2cace2db38eb99527f67118c9
Agnė Venslavičiūtė <agne.vensl@gmail.com>
2021-06-06 00:27:57 +0300

    init

---
+++ src/config.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET,
+  mysqlConfig: {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send({ error: "Page not found" });
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
